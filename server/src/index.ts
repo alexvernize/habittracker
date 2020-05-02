50157aa9ba5888371f9d756b505b29dbb28854bb
André Ferreira <contato@andreferreira.me>
2020-05-02 13:55:10 -0300

    Add ramda

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import app from "./App";
+import { isNil } from "ramda";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`Server: Listening on :${port}`);
+app.listen(port, (err: Error) => {
+  return isNil(err)
+    ? console.log(`Server: Listening on :${port}`)
+    : console.error(err);
 });
